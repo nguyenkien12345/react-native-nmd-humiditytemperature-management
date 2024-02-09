5c48044974365559a3b30148a3545d729b5d0fc8
Nguyen Trung Kien <nguyenkien11202000@gmail.com>
2024-02-09 20:15:16 +0700

    Upload Project

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+const baseURL = 'https://thcntt3.onrender.com';
+
+const crudAPI = {
+    list: 'list',
+    detail: 'detail',
+    add: 'add-data',
+    update: 'update',
+    delete: 'delete'
+};
+
+const crud = {
+    get: 'GET',
+    post: 'POST',
+    put: 'PUT',
+    delete: 'DELETE'
+};
+
+const commonHeadersAPI = {
+    'Content-type': 'application/json; charset=UTF-8'
+};
+
+module.exports = { baseURL, crudAPI, crud, commonHeadersAPI };
